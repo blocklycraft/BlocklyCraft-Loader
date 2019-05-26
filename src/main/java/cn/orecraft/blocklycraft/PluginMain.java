776049826954f73a8ee3186862950b59223d71fa
hempflower <1993996310@qq.com>
2019-05-26 19:54:23 +0800

    更改包名

---
+++ src/main/java/cn/orecraft/blocklycraft/PluginMain.java
@@ -0,0 +1,28 @@
+package cn.orecraft.blocklycraft;/*
+ * Created on 2019/5/26
+ * Author: MaHua_A
+ * Copyright 2019 by OreCraft Studio
+ * DO NOT MODIFY THESE WORDS
+ */
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+
+public class PluginMain extends JavaPlugin {
+
+    @Override
+    public void onLoad(){
+        Bukkit.getLogger().info("Loading scripts.....");
+    }
+    @Override
+    public void onEnable(){
+
+    }
+    @Override
+    public void onDisable(){
+        Bukkit.getLogger().info("Unloading scripts.....");
+    }
+}
