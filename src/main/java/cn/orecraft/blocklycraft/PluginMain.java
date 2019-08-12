085caaf1988a3c6bda296d17dd85775a32fd6bf8
hempflower <1993996310@qq.com>
2019-08-12 15:26:26 +0800

    将插件实例暴露给脚本

--- src/main/java/cn/orecraft/blocklycraft/PluginMain.java
+++ src/main/java/cn/orecraft/blocklycraft/PluginMain.java
@@ -12,27 +12,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import javax.script.SimpleBindings;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 
 
 public class PluginMain extends JavaPlugin {
     private ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName("Nashorn");
 
     @Override
-    public void onEnable(){
+    public void onEnable() {
         try {
             String scriptPath = "/bundle.js";
-            BufferedReader  reader = new BufferedReader(new InputStreamReader(PluginMain.class.getResourceAsStream(scriptPath),"UTF-8"));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(PluginMain.class.getResourceAsStream(scriptPath), "UTF-8"));
             StringBuffer buffer = new StringBuffer();
             String line;
-            while ((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
                 buffer.append(line);
             }
-            scriptEngine.eval(buffer.toString());
+            SimpleBindings bindings = new SimpleBindings();
+            bindings.put("__plugin",this);
+            scriptEngine.eval(buffer.toString(),bindings);
             reader.close();
         } catch (Exception e) {
             e.printStackTrace();
@@ -40,8 +40,9 @@ public class PluginMain extends JavaPlugin {
             Bukkit.getPluginManager().disablePlugin(this);
         }
     }
+
     @Override
-    public void onDisable(){
+    public void onDisable() {
         Bukkit.getLogger().info("Unloading scripts.....");
     }
 }
